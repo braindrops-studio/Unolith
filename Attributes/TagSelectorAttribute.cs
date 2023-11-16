b466530e13a21914a0d0fb961cf857737fdfbd4e
AmiR Bin <ur.amirbin@gmail.com>
2023-11-16 21:33:32 +0330

    Add TagSelector

---
+++ Attributes/TagSelectorAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BrainDrops.Unolith.Attributes
+{
+    public class TagSelectorAttribute : PropertyAttribute
+    {
+        public bool UseDefaultTagFieldDrawer = false;
+    }
+}
---
+++ Editor/TagSelectorPropertyDrawer.cs
@@ -0,0 +1,21 @@
+﻿using BrainDrops.Unolith.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace BrainDrops.Unolith.Editor
+{
+    [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+    public class TagSelectorPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var oldTag = property.stringValue;
+            var newTag = EditorGUI.TagField(position, label, oldTag);
+
+            if (newTag != oldTag)
+            {
+                property.stringValue = newTag;
+            }
+        }
+    }
+}
