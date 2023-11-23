1a4256da792bae15212f88e83da94681fc83b17f
AmiR Bin <ur.amirbin@gmail.com>
2023-11-23 15:21:10 +0330

    Add GetRandomElement in ArrayExtensions

--- Utility/ArrayExtensions.cs
+++ Utility/ArrayExtensions.cs
@@ -1,7 +1,17 @@
-﻿namespace Braindrops.Unolith.Utility
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Braindrops.Unolith.Utility
 {
-    public class ArrayExtensions
+    public static class Extensions
     {
-        
+        private static Random random = new Random();
+     
+        public static T GetRandomElement<T>(this IEnumerable<T> list)
+        {
+            var enumerable = list.ToList();
+            return !enumerable.Any() ? default(T) : enumerable.ElementAt(random.Next(enumerable.Count()));
+        }
     }
 }
