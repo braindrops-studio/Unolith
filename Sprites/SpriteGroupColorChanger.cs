2314c280b948fad6c0580e024d7f9854cfcb9a9e
AmiR Bin <ur.amirbin@gmail.com>
2023-11-29 00:49:05 +0330

    Add SpriteGroupColorDimmer

---
+++ Sprites/SpriteGroupColorChanger.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Braindrops.Unolith.Sprites
+{
+    public class SpriteGroupColorChanger : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer[] spriteRenderers;
+        [SerializeField] private float duration = .5f;
+        [SerializeField] private Color dimColor;
+
+        public void DimColor()
+        {
+            foreach (var spriteRenderer in spriteRenderers)
+                spriteRenderer.DOColor(dimColor, duration);
+        }
+
+        public void NormalizeColor()
+        {
+            foreach (var spriteRenderer in spriteRenderers)
+                spriteRenderer.DOColor(Color.white, duration);
+        }
+    }
+}
